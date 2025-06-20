463e89d6c4bda1f551410771181cafb9cc23e771
Levelcoder123 <muaz.abdulwahid@barq.dev>
2025-06-20 17:44:03 +0500

    load strip keys from .env file

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -12,12 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 
 from pathlib import Path
+from dotenv import load_dotenv
 from datetime import timedelta
 
 from ecommerce_store.env_utils import get_env_variable
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load .env file
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/

42cde2675bb4183c836dde4f5974fed3139ee0d3
Levelcoder123 <muaazkhan968@gmail.com>
2025-07-03 00:08:38 +0500

    Update manage.py to load .env file

--- manage.py
+++ manage.py
@@ -2,12 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 """Django's command-line utility for administrative tasks."""
 
 
 def main():
     """Run administrative tasks."""
+    load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_store.settings')
     try:
         from django.core.management import execute_from_command_line

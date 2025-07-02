c44d6c92cabddd8f55967399dcb6fd6c4c81087a
Levelcoder123 <muaazkhan968@gmail.com>
2025-07-03 00:27:34 +0500

    Match django secret key to .env file

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable('SECRET_KEY')
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
